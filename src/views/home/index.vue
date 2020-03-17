a6535d27047df24f900b2873975ffe3acce74f82
wangmingda <wang521763@163.com>
2020-03-17 18:01:19 +0800

    完成骨架屏布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="home">
-    首页模块
+  <div class="container">
+    主页
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  components: {}
+  name: 'home'
+
 }
 </script>
 
