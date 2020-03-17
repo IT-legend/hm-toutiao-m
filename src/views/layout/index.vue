19c23842dcb144726a7eaebc371a1aaa8111fd3f
wangmingda <wang521763@163.com>
2020-03-17 17:45:47 +0800

    完成layout组件布局

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>公共布局</div>
+  <div class="container">
+    <!-- 1-头部-采用vant导航组件 -->
+    <van-nav-bar fixed title="黑马偷笑" right-text='搜索' v-if="$route.path !== '/user'" @click-right="$router.push('/search')"></van-nav-bar>
+    <!-- 2-中间-二级路由容器 -->
+    <div class="my-wrapper" :class="{noTop : $route.path === '/user'}">
+      <router-view></router-view>
+    </div>
+    <!-- 3-底部-标签栏组件 -->
+    <van-tabbar route>
+      <van-tabbar-item icon="wap-home" to="/">首页</van-tabbar-item>
+      <van-tabbar-item icon="question" to="/question">问答</van-tabbar-item>
+      <van-tabbar-item icon="video" to="/video">视频</van-tabbar-item>
+      <van-tabbar-item icon="manager" to="/user">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
@@ -8,6 +22,21 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.container{
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .my-wrapper{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    padding-top: 46px;
+    padding-bottom: 50px;
+    box-sizing: border-box;
+    &.noTop{
+      padding-top: 0;
+    }
+  }
+}
 </style>
